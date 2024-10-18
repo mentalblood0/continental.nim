57610b32e0eaf70c7503eac1c4b625616c76a195
Нечепоренко Степан <necheporenko_s_iu@goznak.ru>
2024-10-18 17:04:07 +0300

    handle size automatically

--- File.nim
+++ File.nim
@@ -6,20 +6,13 @@ import std/files
 
 type IntegerEncodingError = object of RangeDefect
 
-func to_seq(i: int, length: Natural): seq[uint8] =
+func to_seq(i: Natural): seq[uint8] =
   var c = i
   while true:
-    if Natural(len(result)) == length:
-      raise new_exception(IntegerEncodingError, "size of " & $i & " exceeds " &
-          $length & " bytes")
     result.add(uint8(c mod 256))
     c = c div 256
     if c == 0:
       break
-
-  let zeros = collect:
-    for j in 1..length-len(result): uint8(0)
-  result &= zeros
 
 converter to_int(bytes: seq[uint8]): BiggestInt =
   var m = 0
@@ -30,6 +23,24 @@ converter to_int(bytes: seq[uint8]): BiggestInt =
       m *= 256
     result += int(b) * m
 
+proc write_natural(f: File, i: Natural) =
+  let payload = i.to_seq
+  do_assert f.write_bytes(@[uint8(payload.len)], 0, 1) == 1
+  do_assert f.write_bytes(payload, 0, payload.len) == payload.len
+
+proc read_natural(f: File): BiggestInt =
+  let size = block:
+    var s = @[uint8(0)]
+    do_assert f.read_bytes(s, 0, 1) == 1
+    Natural s[0]
+
+  result = block:
+    var r: seq[uint8]
+    for i in 1 .. size:
+      r.add 0
+    do_assert f.read_bytes(r, 0, size) == size
+    to_int @r
+
 const raw_path = "../test.bin"
 const path = Path "../test.bin"
 const n = 1234
@@ -37,24 +48,12 @@ const n = 1234
 if not file_exists path:
   remove_file path
 
-proc write(f: File, i: Natural, pos: Natural, size: Natural) =
-  let payload = n.to_seq size
-  do_assert f.write_bytes(payload, pos, size) == size
-
-proc read(f: File, pos: Natural, size: Natural): BiggestInt =
-  var r: seq[uint8]
-  for i in 1 .. size:
-    r.add(0)
-  do_assert f.read_bytes(r, 0, size) == size
-  return to_int(@r)
-
 block:
   let f = open(raw_path, fm_write)
-  f.write(n, 0, 8)
+  f.write_natural n
   close f
 
 block:
   let f = open(raw_path, fm_read)
-  check n == f.read(0, 8)
+  check n == f.read_natural
   close f
-
