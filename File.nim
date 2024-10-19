2e4859e35da8e3bf98eb482b10e99bb11a248564
mentalblood <neceporenkostepan@gmail.com>
2024-10-19 23:47:09 +0300

    reading backwards

--- File.nim
+++ File.nim
@@ -29,6 +29,7 @@ type
   Continent* = ref object
     path*: string
     file: File
+    stack: seq[Natural]
 
   DataKind* = enum
     dkNatural
@@ -46,57 +47,86 @@ proc new_continent*(path: string): Continent =
   result.path = path
   result.file = open(path, fm_read_write)
 
-proc `pos=`(c: Continent, value: int) =
+proc pos(c: Continent): Natural =
+  c.file.get_file_pos
+
+proc `pos=`(c: Continent, value: Natural) =
   c.file.set_file_pos value
+
+proc `rpos=`(c: Continent, value: Natural) =
+  c.file.set_file_pos -value, fsp_end
+
+proc rmove(c: Continent, value: Natural) =
+  c.file.set_file_pos -value, fsp_cur
 
 proc write(c: Continent, t: DataKind) =
   do_assert c.file.write_bytes(@[uint8(t)], 0, 1) == 1
 
+proc read_bytes(c: Continent, n: Natural): seq[uint8] =
+  do_assert c.pos >= n
+  c.rmove n
+  result = block:
+    var r: seq[uint8]
+    for i in 1 .. n:
+      r.add 0
+    do_assert c.file.read_bytes(r, 0, r.len) == r.len
+    r
+  c.rmove n
+
+proc read_chars(c: Continent, n: Natural): string =
+  do_assert c.pos >= n
+  c.rmove n
+  result = block:
+    var r: string
+    for i in 1 .. n:
+      r &= 'a'
+    do_assert c.file.read_chars(r, 0, r.len) == r.len
+    r
+  c.rmove n
+
+proc read_byte(c: Continent): uint8 =
+  c.read_bytes(1)[0]
+
 proc write*(c: Continent, i: Natural, write_type: bool = true) =
   let p = block:
-    let a = i.to_seq
-    var r: seq[uint8]
+    var r = i.to_seq
+    r.add(uint8(r.len))
     if write_type:
       r.add(uint8(dkNatural))
-    r & @[uint8(a.len)] & a
+    r
   do_assert c.file.write_bytes(p, 0, p.len) == p.len
 
 proc read_natural*(c: Continent): Natural =
-  let size = Natural c.file.read_char
-
-  var r: seq[uint8]
-  for i in 1 .. size:
-    r.add 0
-  do_assert c.file.read_bytes(r, 0, size) == size
-  return to_natural @r
+  to_natural c.read_bytes Natural c.read_byte
 
 proc write*(c: Continent, s: string) =
-  c.write dkString
-  c.write(s.len, write_type = false)
   do_assert c.file.write_chars(s, 0, s.len) == s.len
+  c.write(s.len, write_type = false)
+  c.write dkString
 
 proc read_string(c: Continent): string =
   let size = c.read_natural
-
-  var r: string
-  for i in 1 .. size:
-    r.add char(0)
-  do_assert c.file.read_chars(r, 0, size) == size
-  return r
+  c.read_chars size
 
 proc read(c: Continent): Data =
-  case DataKind c.file.read_char
+  case DataKind c.read_byte
   of dkNatural:
     Data(kind: dkNatural, nat: c.read_natural)
   of dkString:
     Data(kind: dkString, str: c.read_string)
+
+proc array(c: Continent) =
+  c.stack.add c.file.get_file_pos
+
+proc `end`(c: Continent) =
+  let begin = c.stack.pop
 
 proc test() =
   template test(payload: untyped, k: untyped) =
     block:
       let c = "../test.bin".new_continent
       c.write payload
-      c.pos = 0
+      c.rpos = 0
       check payload == c.read.k
 
   1234.test nat
